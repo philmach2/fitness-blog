7e1afadc885945f519c3482d6830c6f2c288c90d
Phil Mach <philmach2@icloud.com>
2023-10-26 12:16:18 -0400

    created .env, and connected mongoDB

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+//function that allows me to connect to my DB using Mongoose
+//this function must be called
+const connectDB = async () => {
+  try {
+    //process.env.DB_STRING is an environment variable
+    const conn = await mongoose.connect(process.env.DB_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
+
+//function is getting exported to be able to be used elsewhere
+module.exports = connectDB
