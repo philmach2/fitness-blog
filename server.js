4fece588b4c123cdee633a8df0b81751c8413202
Phil Mach <philmach2@icloud.com>
2023-10-26 18:24:43 -0400

    added Category Schema to model

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
 const app = express()
-const PORT = process.env.PORT
+const expressLayouts = require('express-ejs-layouts')
 const connectDB = require('./config/database')
+const PORT = process.env.PORT
 require('dotenv').config({path: './config/.env'})
 
 connectDB()
 
+app.set('view engine', 'ejs')
+app.set('layout', 'layouts/main')
 app.use(express.urlencoded( {extended: true } ))
 app.use(express.static('public'))
 app.use(expressLayouts)
-app.set('layout', 'layouts/main')
-app.set('view engine', 'ejs')
+app.use(express.json())
 
 const routes = require('./server/routes/workoutRoutes.js')
 app.use('/', routes)
 
-app.listen(process.env.PORT, ()=> console.log(`Listening on port ${PORT}`))
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
+})
